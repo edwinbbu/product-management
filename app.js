df753cf51fc796475fa8a6fbf19b53045a05a113
Edwin Babu <edwinbbu@gmail.com>
2018-11-20 12:11:05 +0530

    redirection to https

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/APM'));
 
+app.use(function(req, res, next) {
+      if ((req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+      } else
+        next();
+    });
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/APM/index.html'));
